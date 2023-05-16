922ba2bd14d9eeb2632431565a2e7df3f239c7ef
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-05-16 03:02:15 +0300

    admin dashboard updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        return view("frontend.home");
     }
 
     public function redirects()
