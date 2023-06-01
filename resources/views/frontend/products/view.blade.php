8f469fde279521c1c69ecabab5b88f25f867126a
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-06-01 23:40:01 +0300

    UI Changes

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -172,14 +172,7 @@
   </div>
 </div>
 
-<div class="container py-4">
-  <div class="copyright">
-    &copy; Copyright <strong><span>Nenyon Graphic</span></strong>. All Rights Reserved
-  </div>
-  <div class="credits">
-   Designed by <a href="">Nenyon Graphic</a>
-  </div>
-</div>
+
 
 
 
